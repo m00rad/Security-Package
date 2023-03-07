3d4684237fb9bda67f75f70832a846d75a3501ce
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-03-07 02:44:51 +0200

    Repeating key

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -6,31 +6,69 @@ using System.Threading.Tasks;
 
 namespace SecurityLibrary
 {
-    /*skjfdhgfiukjhkj
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     * fiskfhskdjfh*/
+    
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            string test;
+            test= Decrypt(cipherText, plainText);
+            if (test[8]==test[0])
+                return test.Substring(0, 8);
+            else
+                return test.Substring(0, 9);
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string PlanText = "";
+            Char PlanedLetter;
+            int CharAsciiValue;
+            cipherText = cipherText.ToLower();
+            key = key.ToLower();
+            key = GenerateRepeatingkey(key, cipherText.Length);
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                if (key[i] == cipherText[i])
+                {
+                    PlanedLetter = (Char)97;
+                }
+                else if (key[i] < cipherText[i])
+                {
+                    PlanedLetter = (Char)(97 + cipherText[i] - key[i]);
+                }
+                else
+                {
+                    CharAsciiValue = 122 - (0 + key[i] - cipherText[i]) + 1;
+                    PlanedLetter = (Char)CharAsciiValue;
+                }
+                PlanText += PlanedLetter.ToString();
+            }
+            return PlanText.ToLower();
+            
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string CipherText = "";
+            Char CipheredLetter ;
+            plainText = plainText.ToLower();
+            key = GenerateRepeatingkey(key, plainText.Length);
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                CipheredLetter = (Char)(97 + (((0 + plainText[i] + key[i]) % 97) % 26));
+                CipherText += CipheredLetter.ToString();
+            }
+            return CipherText.ToUpper();
+        }
+        public string GenerateRepeatingkey(string Key,int TextSize)
+        {
+            int KeySize = Key.Length;
+            for (int i = 0; i < TextSize-KeySize; i++)
+            {
+                Key += Key[i % KeySize].ToString();
+            }
+            return Key.ToLower();
         }
     }
 }
