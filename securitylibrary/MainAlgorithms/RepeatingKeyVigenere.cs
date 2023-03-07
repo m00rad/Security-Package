7e57b58792a5c874abeecef86a48e10261c855ba
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-03-07 17:25:54 +0200

    Repeating is Done

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -11,62 +11,70 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            string test;
-            test= Decrypt(cipherText, plainText);
-            if (test[8]==test[0])
-                return test.Substring(0, 8);
-            else
-                return test.Substring(0, 9);
+            string KeyStream, TestKeyStream, Key = "";
+            int PlainTextSize = plainText.Length;
+            KeyStream = Decrypt(cipherText, plainText); 
+            for (int i = 1; i <= PlainTextSize; i++) 
+            {
+                Key = KeyStream.Substring(0, i);
+                TestKeyStream = GenerateRepeatingkey(Key, PlainTextSize);
+                if (TestKeyStream.Equals(KeyStream))
+                    return Key;
+            }
+            return Key;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            string PlanText = "";
-            Char PlanedLetter;
-            int CharAsciiValue;
+            string PlainText = "";
+            Char PlainLetter;
+            int CharASCIIValue;
             cipherText = cipherText.ToLower();
             key = key.ToLower();
             key = GenerateRepeatingkey(key, cipherText.Length);
             for (int i = 0; i < cipherText.Length; i++)
             {
-                if (key[i] == cipherText[i])
+                if (key[i] == cipherText[i])                 // if the two letters are same
                 {
-                    PlanedLetter = (Char)97;
+                    PlainLetter = (Char)97;                  // ASCII value of letter 'a'
                 }
-                else if (key[i] < cipherText[i])
+                else if (key[i] < cipherText[i])             // if the ASCII value of Key's is less than the ASCII of Cipher letter 
                 {
-                    PlanedLetter = (Char)(97 + cipherText[i] - key[i]);
+                    PlainLetter = (Char)(97 + cipherText[i] - key[i]);
                 }
-                else
+                else                                         // the ASCII value of Cipher is less than the ASCII of Key letter
                 {
-                    CharAsciiValue = 122 - (0 + key[i] - cipherText[i]) + 1;
-                    PlanedLetter = (Char)CharAsciiValue;
+                    CharASCIIValue = 122 - (0 + key[i] - cipherText[i]) + 1;
+                    PlainLetter = (Char)CharASCIIValue;
                 }
-                PlanText += PlanedLetter.ToString();
+                PlainText += PlainLetter.ToString();
             }
-            return PlanText.ToLower();
-            
+            return PlainText.ToLower();
         }
 
         public string Encrypt(string plainText, string key)
         {
             string CipherText = "";
-            Char CipheredLetter ;
+            Char CipherLetter ;
             plainText = plainText.ToLower();
             key = GenerateRepeatingkey(key, plainText.Length);
             for (int i = 0; i < plainText.Length; i++)
             {
-                CipheredLetter = (Char)(97 + (((0 + plainText[i] + key[i]) % 97) % 26));
-                CipherText += CipheredLetter.ToString();
+                CipherLetter = (Char)(97 + (((0 + plainText[i] + key[i]) % 97) % 26));
+                CipherText += CipherLetter.ToString();
             }
             return CipherText.ToUpper();
         }
+
         public string GenerateRepeatingkey(string Key,int TextSize)
         {
             int KeySize = Key.Length;
-            for (int i = 0; i < TextSize-KeySize; i++)
+            if (KeySize != TextSize)
             {
-                Key += Key[i % KeySize].ToString();
+                for (int i = 0; i < TextSize - KeySize; i++)
+                {
+                    Key += Key[i % KeySize].ToString();
+                }
             }
             return Key.ToLower();
         }
