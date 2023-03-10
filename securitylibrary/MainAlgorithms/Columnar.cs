a7f1bc97366a5da507c7d0f40cde46c6f13689a0
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-03-10 16:28:30 +0200

    Enc&Dec Colmnar and RailFence

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,17 +10,83 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            List<int> key = new List<int>();
+
+            return key;
         }
 
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
-        }
+            //throw new NotImplementedException();
+            //mainCipher3 = "ctipscoeemrnuce"
+            string plainText = "";
+            double row, column = 0;
+            column = key.Count; // 7 columns
+            SortedDictionary<int, int> keyMapping = new SortedDictionary<int, int>();
 
+            row = Math.Ceiling(cipherText.Length / column); //25/7 = 3.5 ~ 3 
+            char[,] matrix = new char[(int)row, (int)column];
+            int k = 0;
+
+            for (int i = 0; i < column; i++)
+            {
+                keyMapping[key[i]] = i;
+            }
+
+            for (int i = 0; i < column; i++)
+            {
+                foreach (var item in keyMapping)
+                {
+                    for (int j = 0; j < row && k < cipherText.Length; j++)
+                    {
+                        matrix[j, item.Value] = cipherText[k];
+                        k++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    plainText += Char.ToUpper(matrix[i, j]);
+                }
+            }
+            return plainText;
+        }
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //plainText = attackpostponeduntiltwoam
+            string cipherText = "";
+            double row, column = 0;
+            column = key.Count; // 7 columns
+            SortedDictionary<int, int> keyMapping = new SortedDictionary<int, int>();
+
+            row = Math.Ceiling(plainText.Length / column); //25/7 = 3.5 ~ 3 
+            char[,] matrix = new char[(int)row, (int)column];
+            int k = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column && k < plainText.Length; j++)
+                {
+                    matrix[i, j] = plainText[k];
+                    k++;
+                }
+            }
+            for (int i = 0; i < column; i++)
+            {
+                keyMapping[key[i]] = i;
+            }
+            foreach (var item in keyMapping)
+            {
+                for (int r = 0; r < row; r++)
+                {
+                    cipherText += Char.ToUpper(matrix[r, item.Value]);
+                }
+            }
+            return cipherText;
         }
     }
 }
--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,69 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            //  throw new NotImplementedException();
+            int key = 0;
+
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            string plainText = "";
+            double row, column = 0;
+            column = key; // 7 columns
+            row = Math.Ceiling(cipherText.Length / column); //25/7 = 3.5 ~ 3 
+            char[,] matrix = new char[(int)row, (int)column];
+            int k = 0;
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row && k < cipherText.Length; j++)
+                {
+                    matrix[j, i] = cipherText[k];
+                    k++;
+                }
+
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    plainText += Char.ToUpper(matrix[i, j]);
+                }
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            string cipherText = "";
+            double row, column = 0;
+            column = key; // 7 columns
+
+            row = Math.Ceiling(plainText.Length / column); //25/7 = 3.5 ~ 3 
+            char[,] matrix = new char[(int)row, (int)column];
+            int k = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column && k < plainText.Length; j++)
+                {
+                    matrix[i, j] = plainText[k];
+                    k++;
+                }
+            }
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    cipherText += Char.ToUpper(matrix[j, i]);
+                }
+            }
+            return cipherText;
         }
     }
 }
