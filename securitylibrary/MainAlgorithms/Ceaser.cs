8c7535765ac2cf38cf6dd43426739d69e88d814c
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-03-12 17:10:34 +0200

    Ceaser&MonoAlphabetic

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,19 +10,76 @@ namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
+        string alpha = "abcdefghijklmnopqrstuvwxyz";
+        public int Letter_Index(char letter)
+        {
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                if (letter == alpha[i])
+                    return i;
+            }
+            return 0;
+        }
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            char[] characters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] buffer = plainText.ToCharArray();
+            char[] charsofciphertext = new char[plainText.Length];
+            char[] letters = alpha.ToCharArray();
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (buffer[i] == letters[j])
+                    {
+                        int newbit = (key + j) % 26;
+                        char newchar = characters[newbit];
+                        charsofciphertext[i] = newchar;
+                    }
+                }
+            }
+            string cipherMessage = String.Join("", charsofciphertext);
+            return cipherMessage;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            char[] characters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] buffer = cipherText.ToLower().ToCharArray();
+            char[] charsofplaintext = new char[cipherText.ToLower().Length];
+            char[] letters = alpha.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (buffer[i] == letters[j])
+                    {
+                        int newbit = (j - key) % 26;
+                        if (newbit < 0)
+                        {
+                            newbit += 26;
+                        }
+                        char newchar = characters[newbit];
+                        charsofplaintext[i] = newchar;
+                    }
+                }
+            }
+            string plainMessage = String.Join("", charsofplaintext);
+            return plainMessage;
         }
-
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            int key = (Letter_Index(char.ToLower(cipherText[0])) - Letter_Index(plainText[0]));
+            if (key < 0)
+            {
+                key = key + 26;
+            }
+            else
+            {
+                key = key % 26;
+            }
+            return key;
         }
     }
 }
--- securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,21 +11,95 @@ namespace SecurityLibrary
 {
     public class Monoalphabetic : ICryptographicTechnique<string, string>
     {
+        string alpha = "abcdefghijklmnopqrstuvwxyz";
+        public int Letter_Index(char letter)
+        {
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                if (letter == alpha[i])
+                    return i;
+            }
+            return 0;
+        }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            List<int> z = new List<int>();
+            int plain;
+            string key = "";
+            string list = "";
+            List<char> x = plainText.Distinct().ToList();
+            List<char> y = cipherText.ToLower().Distinct().ToList();
+
+            SortedDictionary<char, char> test = new SortedDictionary<char, char>();//(plaintext,cypher)
+
+            string CIPHERText = cipherText.ToLower();
+            for (int i = 0; i < x.Count; i++)
+            {
+                for (int j = 0; j < alpha.Length; j++)
+                {
+                    if (x[i] == alpha[j])
+                    {
+                        test.Add(x[i], y[i]);
+                        //    key += y[i];
+                    }
+                }
+            }
+            if (key.Length < 26)
+            {
+                for (int i = 0; i < 26; i++)
+                {
+                    if (!test.ContainsKey(alpha[i]))
+                    {
+                        for (int j = 0; j < 26; j++)
+                        {
+                            if (!test.ContainsValue(alpha[j]))
+                            {
+                                test.Add(alpha[i], alpha[j]);
+                                j = 26;
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var item in test)
+            {
+                key += item.Value;
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string plainText = "";
+            string CIPHERText = cipherText.ToLower();
+            for (int i = 0; i < CIPHERText.Length; i++)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (CIPHERText[i] == key[j])
+                    {
+                        plainText += alpha[j];
+                    }
+                }
+            }
+            return plainText;
         }
-
         public string Encrypt(string plainText, string key)
-        {
-            throw new NotImplementedException();
-        }
 
+        {
+            string cyphertext = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j < alpha.Length; j++)
+                {
+                    if (plainText[i] == alpha[j])
+                    {
+                        cyphertext += key[j];
+                    }
+                }
+            }
+            return cyphertext;
+        }
         /// <summary>
         /// Frequency Information:
         /// E   12.51%
@@ -56,7 +133,42 @@ namespace SecurityLibrary
         /// <returns>Plain text</returns>
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            throw new NotImplementedException();
+            //Dictionary <string, double>cities = new Dictionary<string, double>() {-
+            // {  "E",12.51} ,   {"T",9.25},{"A",8.04},{ "O",7.60 },{"I",7.26 },{ "N",7.09} ,{"S",6.54 }, {"R",6.12 },{ "H",5.49},{"L",4.14 },{"D",3.99 },{"C",3.06 },{"U",2.71 },{ "M",2.53 } ,{"F",2.30 } ,{"P",2.00 } ,{"G",1.96 } , {"W",1.92},     {"Y",1.73 },{"B",1.54 } ,        {"V",0.99 }  ,      {"K",0.67 },         {"X",0.19 },         {"J",0.16 },       { "Q",0.11 }    ,   {"Z",0.09} 
+            // };
+            int count = 0;
+            cipher = cipher.ToLower();
+            string key = "";
+            Dictionary<char, int> frequencyAlphabiticsDictionary = new Dictionary<char, int>();
+            SortedDictionary<char, char> keydictionary = new SortedDictionary<char, char>();
+            string frequencyalphabitics = "ETAOINSRHLDCUMFPGWYBVKXJQZ".ToLower();
+            int j = 0;
+            while (j < cipher.Length)
+            {
+                if (frequencyAlphabiticsDictionary.ContainsKey(cipher[j]))
+                {
+                    frequencyAlphabiticsDictionary[cipher[j]]++;
+                }
+                else
+                {
+                    frequencyAlphabiticsDictionary.Add(cipher[j], 0);
+                }
+                j++;
+            }
+            frequencyAlphabiticsDictionary = frequencyAlphabiticsDictionary
+                .OrderBy(x => x.Value).Reverse().ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in frequencyAlphabiticsDictionary)
+            {
+                keydictionary.Add(item.Key, frequencyalphabitics[count]);
+                count++;
+            }
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                key = key + keydictionary[cipher[i]];
+            }
+            return key;
         }
     }
 }
+
