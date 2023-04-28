230305937ea7176a76de62b4a70863fb9bca5cb5
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-04-28 20:39:17 +0300

    Update DiffieHellman class

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -1,46 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SecurityLibrary.DiffieHellman
 {
-
     public class DiffieHellman
     {
-
-
-        public int Power(int a, int b, int c)
-        {
-            if (b == 0)
-            {
-                return 1;
-            }
-            else if (b % 2 == 0)
-            {
-                int temp = Power(a, b / 2, c);
-                return (temp * temp) % c;
-            }
-            else
-            {
-                int temp = Power(a, b, c);
-                return temp % c;
-            }
-        }
-
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            int user1 = Power(alpha, xa, q);
-            int user2 = Power(alpha, xb, q);
+            int user1 = power(alpha, xa, q);
+            int user2 = power(alpha, xb, q);
 
             List<int> keys = new List<int>()
-             {
-                Power(user2, xa, q),
-                Power(user1, xb, q)
+            {
+                power(user2, xa, q),
+                power(user1, xb, q)
             };
-
             return keys;
+        }
+        public int power(int a, int b, int c)
+        {
+            int sum = 1;
+            int i = 0;
+            while (i < b)
+            {
+                sum = (sum * a) % c;
+                i++;
+            }
+            return sum;
         }
     }
 }
