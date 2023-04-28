d9a021a4b467cc5e8f1d05c9fc3e93c9bccfca0a
Morad Omar <mourad20191700613@cis.asu.edu.eg>
2023-04-28 04:21:03 +0300

    Enhance ElGamal algorithm

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -21,39 +21,64 @@ namespace SecurityLibrary.ElGamal
         
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-            BigInteger c1, c2;
-            BigInteger K = (BigInteger.Pow(y, k) % q);    
-            c1 = BigInteger.Pow(alpha, k) % q;
+            double c1, c2;
+            double K = PowerWithMod(y, k,q) ;    
+            c1 = PowerWithMod(alpha, k,q);
             c2 = K * m % q;
             List<long> list = new List<long> { (long)c1,(long)c2};
             return list;
 
         }
+        public int PowerWithMod(int Base, int Power, int Mod)
+        {
+            int result= 1;
+            for (int i = 0; i < Power; i++)
+            {
+                result *= Base;
+                result %= Mod;
+            }
+            return result;
+        }
+        public int GCD(int Num1, int Num2)
+        {
+            int result = 1;
+            int loops;
+            if (Num1 > Num2)
+                loops = Num1 / 2;
+            else
+                loops = Num2 / 2;
+            for (int Divisor = 2; Divisor <= loops; Divisor++)
+            {
+                if (Num1 % Divisor == 0 && Num2 % Divisor == 0)
+                    result = Divisor;
+            }
+            return result;
+        }
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            BigInteger k = BigInteger.Pow(c1, x)%q;
-            BigInteger gcd = BigInteger.GreatestCommonDivisor(k, q);
-            if(gcd!=1)
+            int k = PowerWithMod(c1, x, q);
+            int gcd = GCD(k, q);
+            if (gcd != 1)
             {
                 return 0;
             }
-            int x1 = 1, x2 = 0, y1 = 0, y2 = 1,q1 = q;
+            int x1 = 1, x2 = 0, x3, y1 = 0, y2 = 1, y3, q1 = q, c, r;
             while (q1 != 0)
             {
-                double c = Math.Floor((double)(k / q1));
-                double r = (double)k % q1;
+                c = (int)Math.Floor(((double)k / q1));
+                r = k % q1;
                 k = q1;
-                q1 = (int)r;
-                int x3 = x1 - (int)c * x2;
+                q1 = r;
+                x3 = x1 - c * x2;
                 x1 = x2;
                 x2 = x3;
-                int y3 = y1 - (int)c * y2;
+                y3 = y1 - c * y2;
                 y1 = y2;
                 y2 = y3;
             }
-            int m = (c2 * x1) % q;
-            return m;
-
+            return (c2 * x1) % q; //int m = (c2 * x1) % q;
         }
+
     }
+    
 }
